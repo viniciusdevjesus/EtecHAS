96ee310bd13e39977f6cecceb859b42adadfd729
Vinicius-e-Caio <viniciusjesusfilho05@gmail.com>
2024-08-16 10:12:39 -0300

    Feat: Add new student (todo)

--- 2Sem/Python/A2_dicio/Project/NSA.py
+++ 2Sem/Python/A2_dicio/Project/NSA.py
@@ -1,3 +1,20 @@
+from os import system
+from time import sleep
+system("cls")
+
+# Functions
+def VerifyThis(NewData):
+    if isinstance(NewData, str):
+        return NewData
+    if isinstance(NewData, int):
+        return NewData
+    
+    return "invalid"
+    
+
+# Dicts / Variables
+notas = {}
+
 print("Bem vindo ao mini NSA")
 print( """
     1 - Adicionar novo Aluno | Nota (limite 10 alunos)
@@ -13,6 +30,72 @@ while key != 0:
     option = int(input("Digite sua Opção: "))
     match option:
         case 1:
-            print("As vozes não param")
+            # Prints
+            system("cls")
+            print("Você escolheu:") 
+            sleep(1)
+            print("1 - Adicionar novo Aluno | Nota")
+            print("Lembre-se o limite é de 10 alunos")
+            print("\n")
+            sleep(1)
+            system("pause")
+            system("cls")
+            # Temp Variables
+            while True:
+                NewName = VerifyThis(input("Aluno: "))
+                if NewName == "invalid":
+                    continue
+                NewGrade = VerifyThis(input("Nota: "))
+                if NewGrade == "invalid":
+                    continue
+                print("Aluno: {NewGrade} \n Nota: {NewName}")
+                while True:
+                    confirm = str(input("Confirm? (S/N) "))
+                    match confirm:
+                        case "S":
+                            notas = {
+                                {NewGrade} : {NewName}
+                            }
+                            info = 200
+                            break
+                        case "N":
+                            break
+                        case _:
+                            print("Digite uma opção válida...")
+                    break
+                while True:
+                    if info != 200:
+                        opt = str(input("Tentar Novamente? (S/N) "))
+                        if opt == "S":
+                            retry = True
+                            break
+                        elif opt == "N":
+                            retry = False
+                            break
+                        else:
+                            print("Digite uma opção válida..")
+                    break
+                if retry == True:
+                    print("Voltando..")
+                    sleep(1)
+                    print("\n")
+                    system("pause")
+                    system("cls")
+                    continue
+                    
+                
+
+
+        case 2:
+            # Prints
+            system("cls")
+            print("Você escolheu:") 
+            sleep(1)
+            print("2 - Editar Aluno | Nota")
+            print("\n")
+            sleep(1)
+            system("pause")
+
+
         case _:
             print("DIgite uma opção válida")
