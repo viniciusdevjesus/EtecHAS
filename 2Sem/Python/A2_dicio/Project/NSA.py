f358fd49462f39f376a60d0ac141d1cbd5908439
Caio Bernardo <caiobernardo.dev@outlook.com>
2024-08-23 20:32:42 -0300

    feat: Student addition system

--- 2Sem/Python/A2_dicio/Project/NSA.py
+++ 2Sem/Python/A2_dicio/Project/NSA.py
@@ -2,22 +2,13 @@ from os import system
 from time import sleep
 system("cls")
 
-# Functions - For tests
-def VerifyThis(NewData):
-    if isinstance(NewData, str):
-        return NewData
-    if isinstance(NewData, int):
-        return NewData
-    
-    return "invalid"
-    
-
 # Dicts / Variables
-test = {}
+notas = {
+}
+
 
 print("Bem vindo ao mini NSA")
 print( """
-    0 - Sair
     1 - Adicionar novo Aluno | Nota (limite 10 alunos)
     2 - Editar Aluno | Nota
     3 - Listar os Alunos
@@ -30,18 +21,6 @@ key = 1
 while key != 0:
     option = int(input("Digite sua Opção: "))
     match option:
-        case 0:
-            # Prints
-            system("cls")
-            print("Você escolheu:") 
-            sleep(1)
-            print("0 - Sair")
-            print("\n")
-            sleep(1)
-            key = 0
-            system("pause")
-            system("cls")
-    
         case 1:
             # Prints
             system("cls")
@@ -52,8 +31,8 @@ while key != 0:
             print("\n")
             sleep(1)
             system("pause")
-            system("cls")
-            
+    
+
         case 2:
             # Prints
             system("cls")
@@ -64,55 +43,6 @@ while key != 0:
             sleep(1)
             system("pause")
 
-        case 3:
-            # Prints
-            system("cls")
-            print("Você escolheu:") 
-            sleep(1)
-            print("2 - Editar Aluno | Nota")
-            print("\n")
-            sleep(1)
-            system("pause")
-
-        case 4:
-            # Prints
-            system("cls")
-            print("Você escolheu:") 
-            sleep(1)
-            print("2 - Editar Aluno | Nota")
-            print("\n")
-            sleep(1)
-            system("pause")
-
-        case 5:
-            # Prints
-            system("cls")
-            print("Você escolheu:") 
-            sleep(1)
-            print("2 - Editar Aluno | Nota")
-            print("\n")
-            sleep(1)
-            system("pause")
-
-        case 6:
-            # Prints
-            system("cls")
-            print("Você escolheu:") 
-            sleep(1)
-            print("2 - Editar Aluno | Nota")
-            print("\n")
-            sleep(1)
-            system("pause")
-
-        case 7:
-            # Prints
-            system("cls")
-            print("Você escolheu:") 
-            sleep(1)
-            print("2 - Editar Aluno | Nota")
-            print("\n")
-            sleep(1)
-            system("pause")
 
         case _:
             print("DIgite uma opção válida")
